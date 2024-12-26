8cc5a85e09fb6315fc5feafdaf39c910aa09d441
junwon <gkatn3@naver.com>
2024-12-26 15:06:24 +0900

    mvc1

--- src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV4HandlerAdapter.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV4HandlerAdapter.java
@@ -1,7 +1,7 @@
 package hello.servlet.web.frontcontroller.v5.adapter;
 
 import hello.servlet.web.frontcontroller.ModelView;
-import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
 import hello.servlet.web.frontcontroller.v5.MyHandlerAdapter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -9,23 +9,26 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
+public class ControllerV4HandlerAdapter implements MyHandlerAdapter {
 
     @Override
     public boolean supports(Object handler) {
-        // ControllerV3 을 처리할 수 있는 어댑터를 뜻한다.
-        return (handler instanceof ControllerV3);
+        return handler instanceof ControllerV4;
     }
 
     @Override
     public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) {
 
-        // handler를 컨트롤러 V3로 변환한 다음에 V3 형식에 맞도록 호출한다.
-        // supports()를 통해 ControllerV3만 지원하기 때문에 타입 변환은 걱정없이 실행해도 된다.
-        // ControllerV3는 ModelView 를 반환하므로 그대로 ModelView를 반환하면 된다.
-        ControllerV3 controller = (ControllerV3) handler;
+        ControllerV4 controller = (ControllerV4) handler;
+
         Map<String, String> paramMap = createParamMap(request);
-        ModelView mv = controller.process(paramMap);
+        Map<String, Object> model = new HashMap<>();
+
+        String viewName = controller.process(paramMap, model);
+
+        ModelView mv = new ModelView(viewName);
+        mv.setModel(model);
+
         return mv;
     }
 
